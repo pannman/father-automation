04b08ece92c3053397dda071933f45bddd79b0e5
pannman <30381819+pannman@users.noreply.github.com>
2021-04-17 18:37:12 +0900

    取り敢えず一個め完成

--- automation/automation.py
+++ automation/automation.py
@@ -1,11 +1,68 @@
 from selenium import webdriver
 import sys
+import time
+import datetime
 import login_config as LOGIN
 import text_config as TEXT
+import all_config as CONFIG
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+#関数一覧
+def return_r_sub_total_file(zone):
+    if zone == "日中":
+        return r_day_sub_total
+    if zone == "前場":
+        return r_before_sub_total
+    if zone == "後場":
+        return r_after_sub_total
+
+def return_r_main_total_file(zone):
+    if zone == "日中":
+        return r_day_main_total
+    if zone == "前場":
+        return r_before_main_total
+    if zone == "後場":
+        return r_after_main_total
+
+def return_w_sub_total_file(zone):
+    if zone == "日中":
+        return w_day_sub_total
+    if zone == "前場":
+        return w_before_sub_total
+    if zone == "後場":
+        return w_after_sub_total
+
+def return_w_main_total_file(zone):
+    if zone == "日中":
+        return w_day_main_total
+    if zone == "前場":
+        return w_before_main_total
+    if zone == "後場":
+        return w_after_main_total
+
+def return_yes_hour(zone):
+    if zone == "日中":
+        return day_yes_hour
+    if zone == "前場":
+        return before_yes_hour
+    if zone == "後場":
+        return after_yes_hour
+
+def return_yes_minute(zone):
+    if zone == "日中":
+        return day_yes_minute
+    if zone == "前場":
+        return before_yes_minute
+    if zone == "後場":
+        return after_yes_minute
+
+def all_delete(ele):
+    ele.send_keys(Keys.CONTROL,"a")
+    ele.send_keys(Keys.DELETE)
+
 # Chrome のオプションを設定する
 options = webdriver.ChromeOptions()
 
@@ -15,46 +72,114 @@ driver = webdriver.Remote(
     options=options,
 )
 
-
-driver.get('https://fc2.com/login.php?ref=blog')
-print(driver.current_url)
 wait = WebDriverWait(driver, 10)
 driver.implicitly_wait(10)
 driver.set_window_size('1200', '1000')
-args = sys.argv
-r_main_total = open('main_total.txt', 'r').read()
-r_sub_total = open('sub_total.txt', 'r').read()
-w_main_total = open('main_total.txt', 'w')
-w_sub_total = open('sub_total.txt', 'w')
+
+#時間取得
+yes_date = datetime.datetime.now() + datetime.timedelta(days = 1)
+yes_year = str(yes_date.year)
+yes_month = str(yes_date.month)
+yes_day = str(yes_date.day)
+
+day_yes_hour = "6"
+before_yes_hour = "6"
+after_yes_hour = "12"
+
+day_yes_minute = "30"
+before_yes_minute = "35"
+after_yes_minute = "00"
+
+yes_second = "00"
+
+# 日中累計読み込み
+r_day_main_total = open('day_main_total.txt', 'r').read()
+r_day_sub_total = open('day_sub_total.txt', 'r').read()
+w_day_main_total = open('day_main_total.txt', 'w')
+w_day_sub_total = open('day_sub_total.txt', 'w')
+# 前場累計読み込み
+r_before_main_total = open('before_main_total.txt', 'r').read()
+r_before_sub_total = open('before_sub_total.txt', 'r').read()
+w_before_main_total = open('before_main_total.txt', 'w')
+w_before_sub_total = open('before_sub_total.txt', 'w')
+# 後場累計読み込み
+r_after_main_total = open('after_main_total.txt', 'r').read()
+r_after_sub_total = open('after_sub_total.txt', 'r').read()
+w_after_main_total = open('after_main_total.txt', 'w')
+w_after_sub_total = open('after_sub_total.txt', 'w')
+
+# 時間配列に格納
+zones = ['日中', '前場', '後場']
 
 try:
-    if not len(args) == 3:
-        print("サブサインとメインサインを入力してください")
-        raise Exception
+    #fcsログイン
+    driver.get('https://fc2.com/login.php?ref=blog')
+    print(driver.current_url)
     mail = wait.until(EC.presence_of_element_located((By.ID, "id")))
     password = wait.until(EC.presence_of_element_located((By.ID, "pass")))
     login_btn = wait.until(EC.presence_of_element_located((By.NAME, "image")))
-    prevmail = mail.get_attribute('input_fc2id_login')
     mail.send_keys(LOGIN.FC2_LOGIN['ID'])
     password.send_keys(LOGIN.FC2_LOGIN['PASS'])
     login_btn.click()
     print(driver.current_url)
-    driver.get('https://admin.blog.fc2.com/control.php?mode=editor&process=new')
-    print(driver.current_url)
-    title = wait.until(EC.presence_of_element_located((By.ID, "entry_title")))
-    title.send_keys("日中")
-    main_text = wait.until(EC.presence_of_element_located((By.ID, "body")))
-    main_text.send_keys(TEXT.fc2_text("日中",args[1],args[2],r_sub_total,r_main_total))
-    buttun = driver.find_element_by_class_name("admin_common_positive_btn").click()
-    print(r_sub_total)
-    print(TEXT.fc2_text("日中",args[1],args[2],r_sub_total,r_main_total))
-    w_sub_total.write(str(int(r_sub_total) - int(args[1])))
-    w_main_total.write(str(int(r_main_total) - int(args[2])))
+
+    #ブログ投稿
+    for zone in zones:
+        driver.get('https://admin.blog.fc2.com/control.php?mode=editor&process=new')
+        print(driver.current_url)
+
+        #タイトル
+        title = wait.until(EC.presence_of_element_located((By.ID, "entry_title")))
+        title.send_keys(TEXT.fc2_title(zone))
+
+        #テキスト
+        main_text = wait.until(EC.presence_of_element_located((By.ID, "body")))
+        main_text.send_keys(TEXT.fc2_text(zone,CONFIG.sub_sign(zone),CONFIG.main_sign(zone),return_r_sub_total_file(zone),return_r_main_total_file(zone)))
+
+        #予約ラジオボタン
+        reserve_radio = wait.until(EC.presence_of_element_located((By.ID, "entry_property3"))).click()
+        # reserve_radio = driver.find_element_by_id("entry_property3").send_keys(Keys.SPACE)
+
+        #予約時間
+        input_year = driver.find_element_by_name("entry[year]")
+        input_month = driver.find_element_by_name("entry[month]")
+        input_day = driver.find_element_by_name("entry[day]")
+        input_hour = driver.find_element_by_name("entry[hour]")
+        input_minute = driver.find_element_by_name("entry[minute]")
+        input_second = driver.find_element_by_name("entry[second]")
+        dates = [input_year, input_month, input_day, input_hour, input_minute, input_second]
+        for date in dates:
+            all_delete(date)
+        input_year.send_keys(yes_year)
+        input_month.send_keys(yes_month)
+        input_day.send_keys(yes_day)
+        input_hour.send_keys(return_yes_hour(zone))
+        input_minute.send_keys(return_yes_minute(zone))
+        input_second.send_keys(yes_second)
+        
+        # input_hour = driver.find_element_by_class_name("entry[hour]")
+        buttun = driver.find_element_by_class_name("admin_common_positive_btn").click()
+        time.sleep(2)
+
+        # 累計読み込み
+        return_w_sub_total_file(zone).write(str(int(return_r_sub_total_file(zone)) - int(CONFIG.sub_sign(zone))))
+        return_w_main_total_file(zone).write(str(int(return_r_main_total_file(zone)) - int(CONFIG.main_sign(zone))))
+
+        print(TEXT.fc2_text(zone,CONFIG.sub_sign(zone),CONFIG.main_sign(zone),return_r_sub_total_file(zone),return_r_main_total_file(zone)))
+
 except Exception as e:
     print(e)
-    w_sub_total.write(r_sub_total)
-    w_main_total(r_main_total)
+
+    # 日中累計書き込み
+    w_day_sub_total.write(r_day_sub_total)
+    w_day_main_total.write(r_day_main_total)
+    # 前場累計書き込み
+    w_before_sub_total.write(r_before_sub_total)
+    w_before_main_total.write(r_before_main_total)
+    # 後場累計書き込み
+    w_after_sub_total.write(r_after_sub_total)
+    w_after_main_total.write(r_after_main_total)
+
     driver.quit()
 finally:
     driver.quit()
-    
