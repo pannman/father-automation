b25432bc6563f4e7f71baefe00513b3387929eeb
pannman <30381819+pannman@users.noreply.github.com>
2021-05-01 14:01:41 +0900

    ミスの修正

--- automation/package/fc2/megami.py
+++ automation/package/fc2/megami.py
@@ -29,31 +29,31 @@ class Megami(Fc2):
     
     def get_sub_result(self,zone):
         if CONFIG.megami_sub(zone) == "勝ち":
-            self.sub_total+= int(CONFIG.nikkei_mini_result(zone))
+            self.sub_total+= int(CONFIG.nikkei_result(zone))
             self.sub_total = "+" + str(self.sub_total) if self.sub_total > 0 else "±" + str(self.sub_total) if self.sub_total == 0 else str(self.sub_total)
-            return "+" + CONFIG.nikkei_mini_result(zone)
+            return "+" + CONFIG.nikkei_result(zone)
         if CONFIG.megami_sub(zone) == "負け":
-            self.sub_total-= int(CONFIG.nikkei_mini_result(zone))
+            self.sub_total-= int(CONFIG.nikkei_result(zone))
             self.sub_total = "+" + str(self.sub_total) if self.sub_total > 0 else "±" + str(self.sub_total) if self.sub_total == 0 else str(self.sub_total)
-            return "-" + CONFIG.nikkei_mini_result(zone)
+            return "-" + CONFIG.nikkei_result(zone)
         if CONFIG.megami_sub(zone) == "引き分け":
-            self.sub_total+= int(CONFIG.nikkei_mini_result(zone))
+            self.sub_total+= int(CONFIG.nikkei_result(zone))
             self.sub_total = "+" + str(self.sub_total) if self.sub_total > 0 else "±" + str(self.sub_total) if self.sub_total == 0 else str(self.sub_total)
-            return "±" + CONFIG.nikkei_mini_result(zone)
+            return "±" + CONFIG.nikkei_result(zone)
 
     def get_main_result(self,zone):
         if CONFIG.megami_main(zone) == "勝ち":
-            self.main_total+= int(CONFIG.nikkei_mini_result(zone))
+            self.main_total+= int(CONFIG.nikkei_result(zone))
             self.main_total = "+" + str(self.main_total) if self.main_total > 0 else "±" + str(self.main_total) if self.main_total == 0 else str(self.main_total)
-            return "+" + CONFIG.nikkei_mini_result(zone)
+            return "+" + CONFIG.nikkei_result(zone)
         if CONFIG.megami_main(zone) == "負け":
-            self.main_total-= int(CONFIG.nikkei_mini_result(zone))
+            self.main_total-= int(CONFIG.nikkei_result(zone))
             self.main_total = "+" + str(self.main_total) if self.main_total > 0 else "±" + str(self.main_total) if self.main_total == 0 else str(self.main_total)
-            return "-" + CONFIG.nikkei_mini_result(zone)
+            return "-" + CONFIG.nikkei_result(zone)
         if CONFIG.megami_main(zone) == "引き分け":
-            self.main_total+= int(CONFIG.nikkei_mini_result(zone))
+            self.main_total+= int(CONFIG.nikkei_result(zone))
             self.main_total = "+" + str(self.main_total) if self.main_total > 0 else "±" + str(self.main_total) if self.main_total == 0 else str(self.main_total)
-            return "±" + CONFIG.nikkei_mini_result(zone)
+            return "±" + CONFIG.nikkei_result(zone)
 
     def get_main_total_file(self,zone):
         if zone == "日中":
